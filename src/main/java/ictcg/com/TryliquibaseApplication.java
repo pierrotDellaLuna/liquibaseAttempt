caf617798a6e9640d6092c06c1e15054eedf7490
https://github.com/pierrotDellaLuna/lab3.git <>
2019-05-03 15:15:51 +0200

    for the first lift to master

---
+++ src/main/java/ictcg/com/TryliquibaseApplication.java
@@ -0,0 +1,13 @@
+package ictcg.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryliquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryliquibaseApplication.class, args);
+	}
+
+}
